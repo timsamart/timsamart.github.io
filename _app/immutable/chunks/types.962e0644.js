1d249692e50e4a8632da2d39cadc01979a681417
timsamart <timos@outlook.com>
2024-04-12 22:28:56 +0000

    Deploying to gh-pages from @ timsamart/timsamart.github.io@9006f06c6a1d92ad4455e2954e61180d09277f2b 🚀

---
+++ _app/immutable/chunks/types.962e0644.js
@@ -0,0 +1,1 @@
+var i=(e=>(e.GitHub="github",e.StackOverflow="stackoverflow",e.Twitter="twitter",e.Linkedin="linkedin",e.Email="email",e.Facebook="facebook",e.Youtube="youtube",e.Link="link",e))(i||{}),n=(e=>(e.FullTime="Full-time",e.PartTime="Part-time",e.SelfEmployed="Self-employed",e.Freelance="Freelance",e.Contract="Contract",e.Internship="Internship",e))(n||{});export{n as C,i as P};
